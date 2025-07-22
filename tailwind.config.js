a93565977a509a0ccfe846338cac6bb93b44a15f
SarthakSri98 <sarthaksri1998@gmail.com>
2025-07-22 21:12:38 +0530

    feat/tailwind-css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-import type { Config } from "tailwindcss";
 import typography from '@tailwindcss/typography';
 import animate from "tailwindcss-animate";
 
-const config: Config = {
+const config = {
 	content: [
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
