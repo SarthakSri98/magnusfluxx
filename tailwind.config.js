07f7e3448d1d0c985088f52124f264c12f9aeb62
SarthakSri98 <sarthaksri1998@gmail.com>
2025-07-22 21:21:04 +0530

    feat/tailwind-css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,142 +1,134 @@
 import typography from '@tailwindcss/typography';
-import animate from "tailwindcss-animate";
+import animate from 'tailwindcss-animate';
 
 const config = {
 	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 	],
-	darkMode: ['class', "class"],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
-				inter: [
-					'var(--font-inter)'
-				],
-				'open-sans': [
-					'var(--font-open-sans)'
-				]
+				inter: ['var(--font-inter)'],
+				'open-sans': ['var(--font-open-sans)'],
 			},
 			colors: {
 				primary: {
-					'50': '#f0f9ff',
-					'100': '#e0f2fe',
-					'200': '#bae6fd',
-					'300': '#7dd3fc',
-					'400': '#38bdf8',
-					'500': '#0ea5e9',
-					'600': '#0284c7',
-					'700': '#0369a1',
-					'800': '#075985',
-					'900': '#0c4a6e',
-					'950': '#082f49',
+					50: '#f0f9ff',
+					100: '#e0f2fe',
+					200: '#bae6fd',
+					300: '#7dd3fc',
+					400: '#38bdf8',
+					500: '#0ea5e9',
+					600: '#0284c7',
+					700: '#0369a1',
+					800: '#075985',
+					900: '#0c4a6e',
+					950: '#082f49',
 					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))'
+					foreground: 'hsl(var(--primary-foreground))',
 				},
 				slate: {
-					'950': '#0B1121'
+					950: '#0B1121',
 				},
 				indigo: {
-					'400': '#818CF8',
-					'600': '#4F46E5',
-					'700': '#4338CA'
+					400: '#818CF8',
+					600: '#4F46E5',
+					700: '#4338CA',
 				},
 				background: 'hsl(var(--background))',
 				foreground: 'hsl(var(--foreground))',
 				card: {
 					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))'
+					foreground: 'hsl(var(--card-foreground))',
 				},
 				popover: {
 					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))'
+					foreground: 'hsl(var(--popover-foreground))',
 				},
 				secondary: {
 					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))'
+					foreground: 'hsl(var(--secondary-foreground))',
 				},
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))'
+					foreground: 'hsl(var(--muted-foreground))',
 				},
 				accent: {
 					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))'
+					foreground: 'hsl(var(--accent-foreground))',
 				},
 				destructive: {
 					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))'
+					foreground: 'hsl(var(--destructive-foreground))',
 				},
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
 				chart: {
-					'1': 'hsl(var(--chart-1))',
-					'2': 'hsl(var(--chart-2))',
-					'3': 'hsl(var(--chart-3))',
-					'4': 'hsl(var(--chart-4))',
-					'5': 'hsl(var(--chart-5))'
-				}
+					1: 'hsl(var(--chart-1))',
+					2: 'hsl(var(--chart-2))',
+					3: 'hsl(var(--chart-3))',
+					4: 'hsl(var(--chart-4))',
+					5: 'hsl(var(--chart-5))',
+				},
 			},
 			typography: {
 				DEFAULT: {
 					css: {
-						'code::before': {
-							content: '"'
-						},
-						'code::after': {
-							content: '"'
-						}
-					}
-				}
+						'code::before': { content: '""' },
+						'code::after': { content: '""' },
+					},
+				},
 			},
 			animation: {
 				'gradient-x': 'gradient-x 15s ease infinite',
 				'gradient-y': 'gradient-y 15s ease infinite',
-				'gradient-xy': 'gradient-xy 15s ease infinite'
+				'gradient-xy': 'gradient-xy 15s ease infinite',
 			},
 			keyframes: {
-				'gradient-y': {
-					'0%, 100%': {
-						'background-size': '400% 400%',
-						'background-position': 'center top'
-					},
-					'50%': {
-						'background-size': '200% 200%',
-						'background-position': 'center center'
-					}
-				},
 				'gradient-x': {
 					'0%, 100%': {
 						'background-size': '200% 200%',
-						'background-position': 'left center'
+						'background-position': 'left center',
 					},
 					'50%': {
 						'background-size': '200% 200%',
-						'background-position': 'right center'
-					}
+						'background-position': 'right center',
+					},
+				},
+				'gradient-y': {
+					'0%, 100%': {
+						'background-size': '400% 400%',
+						'background-position': 'center top',
+					},
+					'50%': {
+						'background-size': '200% 200%',
+						'background-position': 'center center',
+					},
 				},
 				'gradient-xy': {
 					'0%, 100%': {
 						'background-size': '400% 400%',
-						'background-position': 'left center'
+						'background-position': 'left center',
 					},
 					'50%': {
 						'background-size': '200% 200%',
-						'background-position': 'right center'
-					}
-				}
+						'background-position': 'right center',
+					},
+				},
 			},
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
-			}
-		}
+				sm: 'calc(var(--radius) - 4px)',
+			},
+		},
 	},
 	plugins: [typography, animate],
-}
+};
 
 export default config;
